cb24f2d8f3939805be8ceff08ccc2c611d1e477f
Milkey Tan <819774989@qq.com>
2021-03-13 20:44:52 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
 
 namespace oh_my_arashi
 {
@@ -6,7 +8,25 @@ namespace oh_my_arashi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Oh-My-Arashi");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Console.WriteLine("当前还不支持在 Windows 环境下运行。");
+                Console.WriteLine("Currently not support Windows.");
+                return;
+            }
+
+            if (getuid() != 0)
+            {
+                Console.WriteLine("请使用 Root 权限或 Sudo 运行本程序。");
+                Console.WriteLine("Please use Root permission or Sudo to run. ");
+                return;
+            }
+
+            
         }
+
+        [DllImport("libc")]
+        public static extern uint getuid();
     }
 }
