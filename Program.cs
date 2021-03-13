636eceb6fd2ae259fa30efc7aa671a49013b3f5b
Milkey Tan <819774989@qq.com>
2021-03-13 20:52:21 +0800

    sth

--- Program.cs
+++ Program.cs
@@ -9,17 +9,26 @@ namespace oh_my_arashi
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Oh-My-Arashi");
+            Console.WriteLine("-------------------");
+            var color = Console.ForegroundColor;
+
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("当前还不支持在 Windows 环境下运行。");
                 Console.WriteLine("Currently not support Windows.");
+                Console.ForegroundColor = color;
+                Console.WriteLine("-------------------");
                 return;
             }
 
             if (getuid() != 0)
             {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("请使用 Root 权限或 Sudo 运行本程序。");
                 Console.WriteLine("Please use Root permission or Sudo to run. ");
+                Console.ForegroundColor = color;
+                Console.WriteLine("-------------------");
                 return;
             }
 
